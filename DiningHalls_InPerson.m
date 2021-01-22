735dc34b2018c706db903564d04f0c8870cf0299
mythriambatipudi <mysterymythri@gmail.com>
2021-01-22 16:10:14 -0500

    Update DiningHalls_InPerson.m

--- DiningHalls_InPerson.m
+++ DiningHalls_InPerson.m
@@ -1,4 +1,4 @@
-function [p] = DiningHalls_InPerson(R_positivity, N_Seats, F, r, V, t_f, N_weeks)
+function [p] = DiningHalls_InPerson(R_positivity, N_Seats, F, r, V, t_f, N_Weeks)
     N_Students = N_Seats*4/36;
     N_InfectedStudents = N_Students * R_positivity;
     D_Mask = 1-F;
@@ -20,6 +20,6 @@ function [p] = DiningHalls_InPerson(R_positivity, N_Seats, F, r, V, t_f, N_weeks
     end
     
     D_Meal = sum(n.*450/60*1);
-    D_Semester = D_Meal*N_meals*7*N_weeks;
+    D_Semester = D_Meal*N_meals*7*N_Weeks;
     p = (1-exp(-D_Semester/100))*100;
 end
