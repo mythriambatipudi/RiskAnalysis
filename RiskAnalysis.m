904d2a25e9e235ef78d73dfc97d39c79ffd03b1d
mythriambatipudi <mysterymythri@gmail.com>
2021-01-22 16:10:20 -0500

    Update RiskAnalysis.m

--- RiskAnalysis.m
+++ RiskAnalysis.m
@@ -1,2 +1,3 @@
 [p, waittime] = TeachingSpaces(0.0074, 492, 0.85, 1.12, 3879054, 1.5, 180)
 p = DiningHalls_InPerson(0.0074, 100, 0, 0.2, 849505, 1, 12)
+[p, waittime] = DiningHalls_MealPickUps(0.0074, 100, 0.75, 0.2, 849505, 10/60, 12)
